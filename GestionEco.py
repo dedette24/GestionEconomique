c3b0c91707d730eb6e8b40861f5e9809769d5165
dedette24 <77748549+dedette24@users.noreply.github.com>
2024-03-30 01:47:18 +0100

    0.0.1
    rien de particulier

--- GestionEco.py
+++ GestionEco.py
@@ -24,20 +24,31 @@ def get_last_row(csv_file):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
         derniere_info = file.readlines()[-1] 
+        print(derniere_info.split(","))
         return derniere_info.split(",")
 
 # Lire le fichier CSV et récupérer la dernière ligne
-infos = get_last_row("GestionArgent\stats.csv")
-print(infos)
+infos_data = get_last_row("stats.csv")
+date, argent_dispo, argent_mois = infos_data
+date_actuelle = datetime.datetime.now().strftime("%H:%M:%S|%Y-%m")
+def afficher_infos(infos_data, date, argent_dispo, argent_mois):
+    if infos_data:
+        argent_dispo = float(argent_dispo)
+        argent_mois = float(argent_mois)
 
-if infos:
-    date, argent_dispo, argent_mois = infos
-    argent_dispo = float(argent_dispo)
-    argent_debut_mois = float(argent_mois)
+        print("Date et heure de la dernière ligne :", date)
+        print("Argent disponible:", argent_dispo)
+        print("Argent au début du mois:", argent_mois)
+    else:
+        print("Le fichier CSV est vide.")
+        
+afficher_infos(infos_data, date, argent_dispo, argent_mois)
 
-    print("Date et heure de la dernière ligne :", date)
-    print("Argent disponible:", argent_dispo)
-    print("Argent au début du mois:", argent_mois)
-else:
-    print("Le fichier CSV est vide.")
+def ajouter_ligne_dernier_minute(csv_file, date_actuelle, argent_restant, argent_mois):
+    with open(csv_file, 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow("\n")
+        writer.writerow([date_actuelle, argent_restant, argent_mois])
+        
+ajouter_ligne_dernier_minute("stats.csv", date_actuelle, argent_dispo, argent_mois)
 
